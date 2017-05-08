812d20f1dc81ef2b84cf77ba147b7f4408e66cec
Kevin <cho.kevin@hotmail.com>
2017-05-08 16:24:37 +0800

    defined relationships in models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508041124) do
+ActiveRecord::Schema.define(version: 20170508053949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,22 @@ ActiveRecord::Schema.define(version: 20170508041124) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer  "quantity"
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "status"
+    t.integer  "account_id"
+    t.decimal  "total_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "products", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Product < ActiveRecord::Base
+list = [
+    [ "10.5", "orange" ],
+    [ "7", "apple" ],
+    [ "8", "banana" ],
+    [ "20", "pineapple" ],
+    [ "25", "pen" ],
+    [ "15.5", "mango" ],
+    [ "40", "durian" ]
+  ]
+
+  list.each do |price, name|
+    Product.create( price: price , name: name)
+  end
+end
